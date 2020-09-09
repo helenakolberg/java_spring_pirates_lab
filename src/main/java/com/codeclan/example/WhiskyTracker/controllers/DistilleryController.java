edd72fb5a19899c999433e428fc2db330a87bdac
Helena <inger.helena@gmail.com>
2020-09-09 15:38:18 +0100

    amended the get route to get distilleries by age

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -26,7 +26,7 @@ public class DistilleryController {
             return new ResponseEntity<>(distilleryRepository.findByRegion(region), HttpStatus.OK);
         }
         if (age != null) {
-            return new ResponseEntity<>(distilleryRepository.findByWhiskiesAge(12), HttpStatus.OK);
+            return new ResponseEntity<>(distilleryRepository.findByWhiskiesAge(age), HttpStatus.OK);
         }
         return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
     }
