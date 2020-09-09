3d28ccd3e0ff0b44b92f5aee815213c85337cf66
Helena <inger.helena@gmail.com>
2020-09-09 14:47:12 +0100

    get route for whiskies by distillery name and age working

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,14 @@ public class WhiskyController {
     WhiskyRepository whiskyRepository;
 
     @GetMapping(value = "/whiskies")
-    public ResponseEntity<List<Whisky>> getAllWhiskies(@RequestParam(name = "year", required = false) Integer year) {
+    public ResponseEntity<List<Whisky>> getAllWhiskies(
+            @RequestParam(name = "year", required = false) Integer year,
+            @RequestParam(name = "distillery", required = false) String distilleryName,
+            @RequestParam(name = "age", required = false) Integer age
+    ) {
+        if (distilleryName != null && age != null) {
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distilleryName, age), HttpStatus.OK);
+        }
         if (year != null) {
             return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
         }
